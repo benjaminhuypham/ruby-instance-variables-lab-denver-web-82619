55334ca8ed45630edc7b524799c424f6acfeef90
Huy Pham <benjaminhuypham@gmail.com>
2019-09-05 16:16:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
-class Dog 
-  def name1 (dogs_name)
-    @this_dog_name = dogs_name 
-  end 
-
-  def name2 
-    @this_dog_name
-  end 
-end 
-
-lassie = Dog.new 
-lassie.name1("Lassie")
-
-puts lassie.name2
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
